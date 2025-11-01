1e7e32b02bffc59c242ce4600d6d4d05507ad260
Himanshu <himanshuraj48512@gmail.com>
2025-11-02 02:07:56 +0530

    Inngest

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -1,8 +1,15 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-dotenv.config({quiet:true});
+dotenv.config({ quiet: true });
 
 export const ENV = {
-    PORT: process.env.PORT,
-    DB_URL: process.env.DB_URL,
-}
+  PORT: process.env.PORT,
+  DB_URL: process.env.DB_URL,
+  INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY,
+  INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY,
+  STREAM_API_KEY: process.env.STREAM_API_KEY,
+  STREAM_API_SECRET: process.env.STREAM_API_SECRET,
+  CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+  CLERK_SECRET_KEY_URL: process.env.CLERK_SECRET_KEY,
+  CLIENT_URL: process.env.CLIENT_URL,
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,21 @@
 import express from "express";
+import cors from "cors";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import { serve } from "inngest/express";
+import { inngest } from "./lib/inngest.js";
+import { functions } from "./lib/inngest.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: ENV.CLIENT_URL,
+    credentials: true,
+  })
+);
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "API IS RUNNING" });
@@ -19,7 +32,7 @@ const startServer = async () => {
       console.log(`http://localhost:3000`);
     });
   } catch (error) {
-    console.error("Error starting the server",error);
+    console.error("Error starting the server", error);
   }
 };
 

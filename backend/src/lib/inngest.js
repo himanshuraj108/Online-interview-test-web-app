79835956b9ca7c2d44a8a25540fa6c32aa8195c2
Himanshu <himanshuraj48512@gmail.com>
2025-11-02 03:15:24 +0530

    backendfixed211253

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({ id: "talent-iq" });
 
@@ -10,15 +11,22 @@ const syncUser = inngest.createFunction(
   async ({ event }) => {
     await connectDB();
 
-    const { id, email_address, first_name, last_name, image_url } = event.data;
+    const { id, email_addresses, first_name, last_name, image_url } = event.data;
 
     const newUser = {
       clerkId: id,
-      email: email_address[0]?.email_address,
+      email: email_addresses[0]?.email_address,
       name: `${first_name || ""} ${last_name || ""}`,
       profileImage: image_url,
     };
+
     await User.create(newUser);
+
+    await upsertStreamUser({
+      id: newUser.clerkId.toString(),
+      name: newUser.name,
+      image: newUser.profileImage,
+    });
   }
 );
 
@@ -30,6 +38,8 @@ const deleteUserFromDB = inngest.createFunction(
 
     const { id } = event.data;
     await User.deleteOne({ clerkId: id });
+
+    await deleteStreamUser(id.toString());
   }
 );
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,38 +1,34 @@
 import express from "express";
 import cors from "cors";
+import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
+
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
-import { serve } from "inngest/express";
-import { inngest } from "./lib/inngest.js";
-import { functions } from "./lib/inngest.js";
+import { inngest, functions } from "./lib/inngest.js";
+
 
 const app = express();
 
+// middleware
 app.use(express.json());
-app.use(
-  cors({
-    origin: ENV.CLIENT_URL,
-    credentials: true,
-  })
-);
+// credentials:true meaning?? => server allows a browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
+
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/health", (req, res) => {
-  res.status(200).json({ msg: "API IS RUNNING" });
+  res.status(200).json({ msg: "api is up and running" });
 });
 
-app.get("/books", (req, res) => {
-  res.status(200).json({ msg: "THis is the books endpoint" });
-});
 
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(ENV.PORT, () => {
-      console.log(`http://localhost:3000`);
-    });
+    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
   } catch (error) {
-    console.error("Error starting the server", error);
+    console.error("💥 Error starting the server", error);
   }
 };
 
